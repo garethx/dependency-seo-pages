499cf39937815b4d64c1bed9a279b4c086affb62
Sara <hey@iamsaravieira.com>
2020-02-21 10:36:09 +0100

    increase

--- plugins/gatsby-source-sandboxes/gatsby-node.js
+++ plugins/gatsby-source-sandboxes/gatsby-node.js
@@ -1,18 +1,8 @@
 const crypto = require('crypto')
 const algoliasearch = require('algoliasearch')
 var uniqid = require('uniqid')
-const fs = require('fs')
 const { addMethods } = require('@algolia/client-common')
 const customBrowse = require('./browse')
-
-const sandboxesFile = './sandboxes.json'
-
-const editedTime = () => {
-  const week = 604800
-  const stats = fs.statSync(sandboxesFile)
-  const seconds = (new Date().getTime() - stats.mtime) / 1000
-  return seconds > week
-}
 
 exports.sourceNodes = async ({ actions }, configOptions) => {
   const { createNode } = actions
@@ -28,7 +18,7 @@ exports.sourceNodes = async ({ actions }, configOptions) => {
     const hits = []
     return index
       .customBrowse({
-        limit: '10000',
+        limit: '30000',
         batch(batch) {
           console.log('getting sandboxes')
           hits.push(...batch)
@@ -37,38 +27,31 @@ exports.sourceNodes = async ({ actions }, configOptions) => {
       .then(() => hits)
   }
 
-  let data = []
-  if (!fs.existsSync(sandboxesFile) || editedTime()) {
-    const hits = await getEverything()
-    console.log('getting info')
+  const hits = await getEverything()
+  console.log('getting info')
 
-    const npmDeps = hits.reduce((accumulator, currentValue) => {
-      currentValue.npm_dependencies.map(dep => {
-        const found = accumulator.find(a => a.dependency === dep.dependency)
+  const npmDeps = hits.reduce((accumulator, currentValue) => {
+    currentValue.npm_dependencies.map(dep => {
+      const found = accumulator.find(a => a.dependency === dep.dependency)
 
-        if (found) {
-          if (found.sandboxes.length > 50) return accumulator
-          found.sandboxes.push(currentValue)
-          return accumulator
-        } else {
-          accumulator.push({
-            dependency: dep.dependency,
-            sandboxes: [currentValue]
-          })
-          return accumulator
-        }
-      })
+      if (found) {
+        if (found.sandboxes.length > 50) return accumulator
+        found.sandboxes.push(currentValue)
+        return accumulator
+      } else {
+        accumulator.push({
+          dependency: dep.dependency,
+          sandboxes: [currentValue]
+        })
+        return accumulator
+      }
+    })
 
-      return accumulator
-    }, [])
+    return accumulator
+  }, [])
 
-    data = npmDeps
-    fs.writeFileSync(sandboxesFile, JSON.stringify(data))
-  } else {
-    data = JSON.parse(fs.readFileSync(sandboxesFile))
-  }
   console.log('creating data')
-  data.forEach(datum =>
+  npmDeps.forEach(datum =>
     createNode({
       ...datum,
       id: uniqid(),

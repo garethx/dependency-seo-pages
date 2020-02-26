8a96b507e23f796f3882716bbf621ef3bfa35ed6
Sara <hey@iamsaravieira.com>
2020-02-26 14:42:30 +0100

    change version

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-    'gatsby-plugin-theme-ui',
     {
       resolve: 'gatsby-source-sandboxes',
       options: {

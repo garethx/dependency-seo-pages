19586e2fa25f49379c618686ce478e3971433790
Danny Ruchtie <danny@codesandbox.io>
2020-04-22 16:18:37 +0200

    restored the 6000

--- plugins/gatsby-source-sandboxes/gatsby-node.js
+++ plugins/gatsby-source-sandboxes/gatsby-node.js
@@ -18,7 +18,7 @@ exports.sourceNodes = async ({ actions }, configOptions) => {
     const hits = []
     return index
       .customBrowse({
-        limit: '10',
+        limit: '6000',
         batch(batch) {
           console.log('getting sandboxes')
           hits.push(...batch)
